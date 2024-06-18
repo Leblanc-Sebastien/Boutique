4f7965d2bff19a2f9495ccd66008f761a0914830
sugiilol <sugii.noob@gmail.com>
2024-06-18 18:54:34 +0200

    template css setuped

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,45 @@
 <script setup lang="ts">
-
+  import TheHeader from './components/Header.vue'
+  import TheFooter from './components/Footer.vue'
+  import Cart from './components/Cart.vue'
+  import Shop from './components/Shop.vue'
 </script>
 
 <template>
-  <h1>Hello World !</h1>
+  <div class="main-container">
+    <TheHeader class="header b1"/>    
+    <Shop class="shop b2"/>
+    <Cart class="cart b3"/>  
+    <TheFooter class="footer b4"/>
+  </div>
 </template>
 
 <style scoped lang="scss">
 @import './assets/main.scss';
+
+.main-container{
+  display: grid;
+  grid-template-areas: "header header" "shop cart" "footer footer";
+  grid-template-columns: 70%;
+  grid-template-rows: 48px auto 48px;
+  min-width: 100vh;
+}
+
+.header{
+  grid-area: header;
+}
+
+.shop{
+  grid-area: shop;
+}
+
+.cart{
+  grid-area: cart;
+}
+
+.footer{
+  grid-area: footer;
+}
+
 
 </style>
