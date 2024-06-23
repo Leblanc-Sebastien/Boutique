3d871c5f059745b671b1c70acfcca845c787c40c
SuGii <sugii.noob@gmail.com>
2024-06-23 07:28:40 +0200

    img added - id added - product interface updated

--- src/data/product.ts
+++ src/data/product.ts
@@ -1,49 +1,57 @@
 export default [
     {
+        id:1,
         title: "Mac Book",
-        image: "src/assets/images/alienware.PNG",
+        image: "src/assets/images/macbook.PNG",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laudantium corrupti saepe quasi officiis",
         price: 1750
     },
     {
+        id:2,
         title: "Asus",
-        image: "src/assets/images/alienware.PNG",
+        image: "src/assets/images/asus.PNG",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laudantium corrupti saepe quasi officiis",
         price: 1800
     },
     {
+        id:3,
         title: "Alienware",
         image: "src/assets/images/alienware.PNG",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laudantium corrupti saepe quasi officiis",
         price: 2700
     },
     {
+        id:4,
         title: "Lenovo",
-        image: "src/assets/images/alienware.PNG",
+        image: "src/assets/images/lenovo.PNG",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laudantium corrupti saepe quasi officiis",
         price: 1590
     },
     {
+        id:5,
         title: "Acer",
-        image: "src/assets/images/alienware.PNG",
+        image: "src/assets/images/acer.PNG",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laudantium corrupti saepe quasi officiis",
         price: 1500
     },
     {
+        id:6,
         title: "HP Envy",
-        image: "src/assets/images/alienware.PNG",
+        image: "src/assets/images/hp.PNG",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laudantium corrupti saepe quasi officiis",
         price: 1200
     },
     {
+        id:7,
         title: "Mac Book pro",
-        image: "src/assets/images/alienware.PNG",
+        image: "src/assets/images/macbookpro.PNG",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laudantium corrupti saepe quasi officiis",
-        price: 1300
+        price: 2600
     },
     {
+        id:8,
         title: "Aspire",
-        image: "src/assets/images/alienware.PNG",
+        image: "src/assets/images/aspire.PNG",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laudantium corrupti saepe quasi officiis",
         price: 1450
     }
