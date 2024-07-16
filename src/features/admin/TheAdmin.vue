ab8082f0e8a4c382ec124c6aaca84a87b6bfb920
SuGii <sugii.noob@gmail.com>
2024-07-16 08:36:08 +0200

    starting to setup the Form to add products

--- src/features/admin/TheAdmin.vue
+++ src/features/admin/TheAdmin.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
-
+import ProductForm from './components/ProductForm.vue'
 </script>
 
 <template>
-    <div class="admin-container">
-        <h1>Admin</h1>
+    <div class="admin-container d-flex flex-column align-item-center justify-content-center">
+        <ProductForm />
     </div>
-   
+
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
---
+++ src/features/admin/components/ProductForm.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { Form, Field } from 'vee-validate';
+
+const onSubmit = (values: string) => {
+    console.log(values)
+}
+</script>
+
+<template>
+    <div class="product-form-container d-flex flex-column align-item-center justify-content-center">
+        <h1>Admin</h1>
+        <Form v-on:submit="onSubmit" class="product-form d-flex flex-column">
+
+            <label for="title">Titre :</label>
+            <Field name="title" type="text" id="title" />
+
+            <label for="description">Description :</label>
+            <Field name="description" type="text" id="description" />
+
+            <label for="img">Image :</label>
+            <Field name="img" type="text" id="img" />
+
+
+
+            <button>Valider</button>
+        </Form>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+.product-form {
+    gap: 10px;
+    background-color: #fff;
+    padding: 40px;
+    border-radius: 5px;
+    margin: 20px;
+}
+</style>
